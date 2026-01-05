48f33eb2450198f4659d9b2e563eaf4206ee8f36
ThomasLeBg94 <147928021+ThomasLeBg94@users.noreply.github.com>
2026-01-05 19:30:59 +0100

    Added Database and pgAdmin to the compose.

--- NodeServer/Scripts/startAll.sh
+++ NodeServer/Scripts/startAll.sh
@@ -7,6 +7,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # Dossier contenant docker-compose.yml
 COMPOSE_DIR="$SCRIPT_DIR/../"
 
+export COMPOSE_FILE="$COMPOSE_DIR/docker-compose.yml"
+
 cd "$COMPOSE_DIR"
 
 echo "Arrêt et nettoyage de l'ensemble de l'application."
@@ -20,4 +22,11 @@ docker compose up -d
 
 echo "------------------------------------------------------"
 echo "Serveur Node disponible à l'url : http://127.0.0.1:80"
+echo "Database PostgreSQL démarrée sur le port 5432"
+echo "PgAdmin disponible à l'url : http://127.0.0.1:5050"
 echo "------------------------------------------------------"
+
+echo "Restoration de la database..."
+cd ./Scripts
+./restoreDB.sh
+echo "Restoration de la database terminée."
