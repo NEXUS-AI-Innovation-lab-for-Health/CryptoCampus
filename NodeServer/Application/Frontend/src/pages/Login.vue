ddd335e97a5d38deace81f272c6eda197d659887
HSEV <h.sevpro@gmail.com>
2026-06-08 16:17:52 +0200

    - ajout du logo linkedin pour mieux reconnaitre le bouton Connexion Linkedin

--- NodeServer/Application/Frontend/src/pages/Login.vue
+++ NodeServer/Application/Frontend/src/pages/Login.vue
@@ -76,7 +76,10 @@
         </div>
 
         <button type="button" class="btn-linkedin" @click="continueWithLinkedIn">
-          🔗 Continuer avec LinkedIn
+          <svg class="linkedin-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+            <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 1 1 0-4.124 2.062 2.062 0 0 1 0 4.124zM7.114 20.452H3.56V9h3.554v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+          </svg>
+          Continuer avec LinkedIn
         </button>
         <div class="divider"><span>ou</span></div>
 
@@ -573,10 +576,20 @@ export default {
   font-size: 1rem;
   cursor: pointer;
   transition: background-color 0.3s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
 }
 
 .btn-linkedin:hover {
   background-color: #084e96;
+}
+
+.linkedin-icon {
+  width: 1.1rem;
+  height: 1.1rem;
+  flex-shrink: 0;
 }
 
 .linkedin-step-intro {
